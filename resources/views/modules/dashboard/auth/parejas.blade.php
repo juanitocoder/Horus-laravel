dbf5b51aad6ddbf1678ac84dcad6099cde4c94f7
juanitocoder <bjuan560@gmail.com>
2025-05-04 18:04:38 -0500

    arreglo botones catalogo

--- resources/views/modules/dashboard/auth/parejas.blade.php
+++ resources/views/modules/dashboard/auth/parejas.blade.php
@@ -2,10 +2,8 @@
 
 @section('parejas')
 
-<h2 class="relative text-3xl font-extrabold text-center mb-8">
-  <span class="bg-clip-text text-transparent bg-gradient-to-r from-purple-400 to-purple-600">Catálogo Parejas</span>
-  <div class="absolute -bottom-3 left-1/2 transform -translate-x-1/2 w-24 h-1 bg-gradient-to-r from-purple-400 to-purple-600 rounded-full"></div>
-  <div class="absolute -bottom-3 left-1/2 transform -translate-x-1/2 w-16 h-1 bg-purple-300 rounded-full opacity-50 animate-pulse"></div>
+<h2 class="relative text-3xl font-extrabold text-center mb-8 text-white font-cinzel">
+  Catálogo Parejas
 </h2>
 
 <x-botones_catalogo />
